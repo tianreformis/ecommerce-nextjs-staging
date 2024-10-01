3c70378c3ed6a4039f07e78ab7b27972364dbfab
tianreformis <tianreformis@gmail.com>
2024-10-01 10:44:55 +0800

    addign components : login components

---
+++ src/components/view/auth/Login/index.tsx
@@ -0,0 +1,14 @@
+const LoginView = () => {
+    return (
+        <div className="h-screen w-screen flex flex-col  justify-center items-center">
+            <div className="bg-slate-200 shadow-md rounded-md h-[400px] w-[300px] sm:w-[600px] flex flex-col  justify-center items-center">
+                <h1 className="text-3xl font-bold">Login Page</h1>
+                
+                
+                </div>
+
+        </div>
+    )
+}
+
+export default LoginView
---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import LoginView from "@/components/view/auth/Login"
+
+const LoginPage = () => {
+    return (
+        <div>
+            <LoginView />
+        </div>
+    )
+}
+
+export default LoginPage
