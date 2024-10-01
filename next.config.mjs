c72a93cd42503feedaabcedc91df20536b99eac2
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-10-01 08:32:59 +0800

    adding json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        'aws-sdk': false,  // Prevents aws-sdk from being bundled in client-side code
+        fs: false,
+        path: false,
+        os: false,
+        'nock': false,  // Ensures 'nock' is not bundled on the client side
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/api/user/register.ts
+++ src/pages/api/user/register.ts
@@ -21,6 +21,8 @@ export default async function handler(
             .status(405)
             .json({ status: false, message: 'method not allowed' });
     }
+    
+    
 
 
 
