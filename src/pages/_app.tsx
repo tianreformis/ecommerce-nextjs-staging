0ff0a424c7043c3b9862266570a1d632ef4078b0
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-10-01 15:56:59 +0800

    adding login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google"
 
@@ -8,10 +9,15 @@ const poppins = Poppins({
   weight: ["300", "400"]
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <div className={poppins.className}>
-      <Component {...pageProps} />
-    </div>
+    <SessionProvider session={session}>
+      <div className={poppins.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+
   )
 }
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { signIn } from "@/lib/firebase/service";
+import { compare } from "bcryptjs";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            type: 'credentials',
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                const { email, password } = credentials as {
+                    email: string;
+                    password: string;
+                };
+
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const user: any = await signIn(email);
+                if (user) {
+                    const passwordConfirm = await compare(password, user.password);
+                    if (passwordConfirm) {
+                        return user;
+                    }
+                    return null;
+                } else {
+                    return null;
+                }
+            }
+        }),
+    ],
+    callbacks: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        async jwt({ token, account, profile, user }: any) {
+            if (account?.provider === "credentials") {
+                token.email = user.email;
+                token.fullname = user.fullname;
+                token.phone = user.phone;
+                token.role = user.role;
+            }
+            return token;
+        },
+        async session({ session, token }: any) {
+            if ("email" in token) {
+                session.user.email = token.email;
+            }
+            if ("fullname" in token) {
+                session.user.fullname = token.fullname;
+            }
+            if ("phone" in token) {
+                session.user.phone = token.phone;
+            }
+            if ("role" in token) {
+                session.user.role = token.role;
+            }
+            return session;
+
+        }
+    },
+
+    pages: {
+        signIn: '/auth/login',
+    },
+}
+
+export default NextAuth(authOptions);
