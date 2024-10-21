cc53c687f6d2c74bea2946008c1bebdf84f7bb9b
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-10-21 15:14:11 +0800

    adding data

--- src/components/view/home/index.tsx
+++ src/components/view/home/index.tsx
@@ -2,16 +2,16 @@ import { NavbarHome } from '@/components/ui/navbar';
 import { ContentHome } from '@/components/ui/content';
 import Head from 'next/head';
 export default function HomeView() {
-    
+
 
     return (
         <>
-        <Head>
-            <title>Tian Store</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <NavbarHome />
-        <ContentHome />
+            <Head>
+                <title>Tian Store</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <NavbarHome />
+            <ContentHome />
         </>
     )
 }
