1111257ae6294a3a8ed0ff96bb1b4ffbcb05b7de
tianreformis <tianreformis@gmail.com>
2024-10-27 20:46:48 -0400

    adding some data to fetch

--- src/components/view/admin/Users/index.tsx
+++ src/components/view/admin/Users/index.tsx
@@ -1,4 +1,5 @@
 import AdminLayout from "@/components/layouts/AdminLayout";
+import Button from "@/components/ui/button";
 import Head from "next/head";
 
 type PropTypes = {
@@ -14,11 +15,11 @@ const AdminUsersView = (props: PropTypes) => {
             <Head>
                 <title>Admin | Users Management </title>
             </Head>
-            <div>
+            <div className="text-xl font-bold">
                 Admin Users Dashboard
             </div>
             <div className="card-table">
-                <table className="table align-middle text-gray-700 font-medium text-sm">
+                <table className="table  align-middle text-gray-700 font-medium text-sm">
                     <thead className="">
                         <tr>
                             <th>#</th>
@@ -26,16 +27,34 @@ const AdminUsersView = (props: PropTypes) => {
                             <th>Email</th>
                             <th>Role</th>
                             <th>Phone</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody className="border border-black">
                         {users.map((user: any, index: number) => (
-                            <tr key={user.id} className="border-black ">
+
+                            <tr key={user.id} className="border-black">
+
                                 <td>{index + 1}</td>
                                 <td>{user.fullname}</td>
                                 <td>{user.email}</td>
-                                <td>{user.role}</td>
+
+                                <td
+
+                                    className={`
+                                        ${user.role === "admin" 
+                                            ? "bg-red-400" 
+                                            : "bg-green-300"
+                                        } px-2 py-1 rounded`}>
+
+                                    {user.role}
+                                </td>
                                 <td>{user.phone}</td>
+                                <td className="flex flex-row gap-2">
+                                    <Button type="button">Edit</Button>
+                                    <Button type="button">Hapus</Button>
+
+                                </td>
 
 
                             </tr>
