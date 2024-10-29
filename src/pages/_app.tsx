ce022e65e45b916e32425d1458065edc8799cfe8
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-10-29 16:11:48 +0800

    adding : hint text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import { NavbarHome } from "@/components/ui/navbar";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import {Montserrat, Poppins } from "next/font/google"
+import {Montserrat, Poppins, Lato as Raleway } from "next/font/google"
 import { useRouter } from "next/router";
 
 const disableNavbar = ['auth', 'admin','404'];
 
 const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400"],
+});
+
+const raleway = Raleway({
   subsets: ["latin"],
   display: "swap",
   weight: ["300", "400"],
@@ -25,7 +31,7 @@ export default function App({ Component,
   const { pathname } = useRouter();
   return (
     <SessionProvider session={session}>
-      <div className={poppins.className && montserrat.className}>
+      <div className={raleway.className}>
         {!disableNavbar.includes(pathname.split('/')[1]) && <NavbarHome />}
         <Component {...pageProps} />
       </div>
