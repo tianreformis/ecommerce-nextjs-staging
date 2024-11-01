9b706954e40cd1f844f1b1db97bf0c39c0069064
tianreformis <tianreformis@gmail.com>
2024-11-01 04:00:04 -0400

    adding some views

--- src/components/view/admin/Users/index.tsx
+++ src/components/view/admin/Users/index.tsx
@@ -79,11 +79,11 @@ const AdminUsersView = (props: PropTypes) => {
                                             {user.role}
                                         </span>
                                     </td>
-                                    <td className="px-6 py-4 text-black">{user.email}</td>
+                                    <td className="px-6 py-4 text-black hover:underline cursor-text">{user.email}</td>
                                     <td className="px-6 py-4">
                                         <div className="flex gap-2">
                                             <span
-                                                className="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600"
+                                                className="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600 hover:font-bold"
                                             >
                                                 {user.phone != "" ? user.phone : "Kosong"}
                                             </span>
